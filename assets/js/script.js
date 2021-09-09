77562a894d8790b2e4caf59dfed75e42a7cfd68b
Adam Tilly <tillyjadam@gmail.com>
2021-09-08 23:00:09 -0500

    fixed movment bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ var taskIdCounter = 0;
 
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
-var tasksInProgressEl = document.querySelector("#tasks-in-progress");
+var tasksInProgressEl = document.querySelector("#task-in-progress");
 var tasksCompletedEl = document.querySelector("#tasks-completed");
 var pageContentEl = document.querySelector("#page-content");
 
